f94a6cdf607c38c0de01184d7ee9b47f4e1eff1f
BWOH <bwoh@outlook.kr>
2025-09-15 00:45:17 +0900

    🎉Project 시작

---
+++ app/src/main/java/kr/ac/kumoh/s20250000/s25w0302composable/MainActivity.kt
@@ -0,0 +1,52 @@
+package kr.ac.kumoh.s20250000.s25w0302composable
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import kr.ac.kumoh.s20250000.s25w0302composable.ui.theme.S25W0302ComposableTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            S25W0302ComposableTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+
+        }
+    }
+}
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    S25W0302ComposableTheme {
+//        Greeting("Android")
+//    }
+//}
